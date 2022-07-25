b94895b2d16f71b961de4b8005270884a43194b5
Megha Bodke <megha.n.bodke@gmail.com>
2022-07-25 13:52:31 +0530

    update css file for hamburger icon

---
+++ portfolio.js
@@ -0,0 +1,13 @@
+/* let h = document.querySelector("#hmbg");
+console.log(h); */
+
+
+/* document.addEventListener('click', function(event){
+    h.cl
+}
+); */
+let hmbg = document.querySelector(".hambrgricon");
+hmbg.addEventListener("click",function(){
+  hmbg.classList.toggle("fa-times");
+
+});
