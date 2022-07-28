301ef0d2c34c54db42296e36518c733685cfe203
Megha Bodke <megha.n.bodke@gmail.com>
2022-07-28 14:50:08 +0530

    updated linter errors

--- portfolio.js
+++ portfolio.js
@@ -1,16 +1,16 @@
-/*Element acess through java script*/
+/* Element acess through java script */
 const hmbg = document.querySelector('.fa-bars');
 const clo = document.querySelector('#closebtn');
 const linkitems = document.querySelectorAll('.linkitems');
 const navbar = document.querySelector('.header2');
 
-/*Event listner for hem icon*/
+/* Event listner for hem icon */
 hmbg.addEventListener('click', () => {
   navbar.style.display = 'block';
   hmbg.style.display = 'none';
 });
 
-/*Event listner for close button*/
+/* Event listner for close button */
 clo.addEventListener('click', () => {
   navbar.style.display = 'none';
   hmbg.style.display = 'block';
