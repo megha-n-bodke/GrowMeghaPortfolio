e0ee965ae66e14e74c2b279cbe35ae84182693f6
Amit Kumar <amitlohan@rediffmail.com>
2022-07-28 21:08:47 +0530

    add comments in local-strage.js

--- local-storage.js
+++ local-storage.js
@@ -1,24 +1,20 @@
+/* const asigned to the element */
 const userName = document.getElementById('username');
 const userEmail = document.getElementById('email');
 const userMsg = document.getElementById('message');
 
-/* console.log(userName,userMsg,userEmail); */
 changeHandler = () => {
   const userData = {
     name: userName.value,
     email: userEmail.value,
     msg: userMsg.value,
   };
-
-  /* console.log(userData.email); */
-  /* const obj = {
-    name: 'microverse',
-  }; */
-
+  
   localStorage.setItem('userInfo', JSON.stringify(userData));
 };
+
 const user = JSON.parse(window.localStorage.getItem('userInfo'));
-/* console.log(user); */
+
 userName.value = user.name;
 userEmail.value = user.email;
 userMsg.value = user.msg;
