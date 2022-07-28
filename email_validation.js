a2e9c21f32a766548c17346123b120a7a0e9e7ae
Megha Bodke <megha.n.bodke@gmail.com>
2022-07-28 22:43:57 +0530

    Revert "updated unneccesary files"
    This reverts commit 0318e0f7432993a29e17289f95fe3dbf1fe13a23.

---
+++ email_validation.js
@@ -0,0 +1,34 @@
+const submit = document.getElementById('gettouch');
+const id = document.getElementById('email');
+const EMAIL_CASE_CHECK = 'Please Enter Email Address In Lower Case';
+const EMAIL_CHECK = 'Please enter valid email address';
+const err = document.getElementById('err-msg');
+
+/* email.insertAdjacentElement('beforebegin', span);
+ */
+
+submit.addEventListener('click', (event) => {
+  const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const email_add = id.value;
+  if (email_add === email_add.toLowerCase()) {
+    if (validRegex.test(email_add)) {
+      err.classList.remove('show-alert');
+      err.classList.add('hide-alert');
+    } else {
+      err.innerHTML = `<span id="err-msg">${EMAIL_CHECK}</span>`;
+      event.preventDefault();
+      err.classList.add('show-alert');
+      err.classList.remove('hide-alert');
+    }
+  } else {
+    event.preventDefault();
+    err.innerHTML = `<span id="err-msg">${EMAIL_CASE_CHECK}</span>`;
+    err.classList.add('show-alert');
+    err.classList.remove('hide-alert');
+  }
+
+  setTimeout(() => {
+    err.classList.remove('show-alert');
+    err.classList.add('hide-alert');
+  }, 2500);
+});
