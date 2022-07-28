3dfcbe1a216cf6d4202df5c14bd6233b7adef6f0
Megha Bodke <megha.n.bodke@gmail.com>
2022-07-28 15:47:08 +0530

    updated linter errors

--- portfolio.js
+++ portfolio.js
@@ -1,11 +1,9 @@
-/*Asign var to the query selector emlement*/
+/* Asign var to the query selector emlement */
 
 const hmbg = document.querySelector('.fa-bars');
 const clo = document.querySelector('#closebtn');
 const linkitems = document.querySelectorAll('.linkitems');
 const navbar = document.querySelector('.header2');
-
-/*add event listner to hem*/
 
 hmbg.addEventListener('click', () => {
   navbar.style.display = 'block';
